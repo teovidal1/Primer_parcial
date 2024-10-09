cd6a5b77ab2ad91b795f501f3f2c268c269c1630
Teo Vidal <vidalteo23@gmail.com>
2024-10-09 11:48:57 -0300

    Parcial Terminado

--- funcionesparcial.py
+++ funcionesparcial.py
@@ -14,7 +14,7 @@ def print_menu ():
         9. SALIR
         """)
     
-def cargar_pacientes (pacientes:list[list])->list[list]:
+def cargar_pacientes (pacientes:list[list]):
     """Función que agrega una cantidad definida de pacientes con numero clinico, nombre, edad, diagnostico y dias de internación a una lista.
 
     Args:
@@ -27,25 +27,26 @@ def cargar_pacientes (pacientes:list[list])->list[list]:
     for i in range (cantidad_a_ingresar):
 
         num_hist_clinica = (input(f"Ingrese el número de historia clínica del {i+1}° paciente: "))
-        while num_hist_clinica.isdigit()==False:
+        while num_hist_clinica.isdigit() == False:
             num_hist_clinica = input(f"ERROR. Ingrese el número de historia clínica del {i+1}° paciente: ")
 
-        num_hist_clinica=int(num_hist_clinica)
+        num_hist_clinica = int(num_hist_clinica)
 
         nombre = input(f"Ingrese el nombre del {i+1}° paciente: ")
 
         edad = input(f"Ingrese la edad del {i+1}° paciente: ")
-        while edad.isdigit()==False or int(edad)<0:
+        while edad.isdigit() == False or int(edad)<0:
             edad = input(f"ERROR.Ingrese la edad del {i+1}° paciente: ")
         edad=int(edad)
 
         diagnostico = input(f"Ingrese el diagnóstico del {i+1}° paciente: ")
 
-        dias_de_internacion = input(f"¿Cuántos días de internacion lleva? del {i+1}° paciente: ")
-        while dias_de_internacion.isdigit()==False or int(dias_de_internacion)<0:
-            dias_de_internacion = input(f"ERROR.¿Cuántos días de internacion lleva? del {i+1}° paciente: ")
-        dias_de_internacion=int(dias_de_internacion)
+        dias_de_internacion = input(f"¿Cuántos días de internacion lleva el {i+1}° paciente?: ")
+        while dias_de_internacion.isdigit() == False or int(dias_de_internacion) < 0:
+            dias_de_internacion = input(f"ERROR.¿Cuántos días de internacion lleva el {i+1}° paciente?: ")
+        dias_de_internacion = int(dias_de_internacion)
         pacientes.append([num_hist_clinica,nombre,edad,diagnostico,dias_de_internacion])
+        print (" ")
 
 def mostrar_lista_pacientes (pacientes:list[list]):
     """Printea la matriz
@@ -65,30 +66,30 @@ def buscar_paciente_x_historia_clinica (pacientes:list[list]):
         pacientes (list[list]): Arreglo bidimensional
     """
     encontrado = False
-    paciente_buscado = input ("Ingrese el numero de historia clínica del paciente: ")
+    paciente_buscado = int(input ("Ingrese el numero de historia clínica del paciente: "))
     for i in range (len(pacientes)):
-        if pacientes[i][0]==paciente_buscado:
+        if pacientes[i][0] == paciente_buscado:
             print (f"""El paciente con número {paciente_buscado} se llama {pacientes[i][1]}, tiene {pacientes[i][2]} años de edad. 
-                Se le diagnosticó {pacientes[i][3]} y está internado hace {pacientes[i][4]} días""")
+            Se le diagnosticó {pacientes[i][3]} y está internado hace {pacientes[i][4]} días""")
             encontrado = True
     if not encontrado:
         print ("NO SE ENCONTRÓ EL PACIENTE BUSCADO.")
 
-def ordenar_pacientes_por_número (pacientes:list[list])->list[list]:
+def ordenar_pacientes_por_número (pacientes:list[list]):
     """Ordena la matriz por su número de historia clinica de cada usuario de forma ascendente
 
     Args:
         pacientes (list[list]): Arreglo bidimensional
     """
-    cantidad_pacientes=len(pacientes)
+    cantidad_pacientes = len(pacientes)
     for i in range (cantidad_pacientes-1):
         for j in range (cantidad_pacientes-1-i):
-            if (pacientes[j][0])>(pacientes[j+1][0]):
+            if (pacientes[j][0]) > (pacientes[j+1][0]):
                 temp = pacientes[j+1]
                 pacientes[j+1] = pacientes[j]
                 pacientes[j] = temp     
 
-def encontrar_paciente_mas_dias (pacientes:list[list])->list[list]:
+def encontrar_paciente_mas_dias (pacientes:list[list]):
     """Busca en la matriz el paciente con más días de internacion y printea sus datos
 
     Args:
@@ -98,17 +99,17 @@ def encontrar_paciente_mas_dias (pacientes:list[list])->list[list]:
     cantidad_pacientes = len(pacientes)
 
     for i in range (cantidad_pacientes):
-        if pacientes[i][4]>paciente_mas_dias:
-            paciente_mas_dias=pacientes[i][4]
+        if pacientes[i][4] > paciente_mas_dias:
+            paciente_mas_dias = pacientes[i][4]
             nombre_paciente_mas_dias = pacientes[i][1]
             numero_historial_paciente_mas_dias = pacientes[i][0]
             edad_paciente_mas_dias = pacientes [i][2]
             diagnostico_paciente_mas_dias = pacientes [i][3]
 
     print (f"""El paciente con más días es {nombre_paciente_mas_dias}, numero clínico {numero_historial_paciente_mas_dias}. Tiene {edad_paciente_mas_dias} años,
-        le diagnosticaron {diagnostico_paciente_mas_dias} y está internado hace {paciente_mas_dias} """)        
+    le diagnosticaron {diagnostico_paciente_mas_dias} y está internado hace {paciente_mas_dias} días """)        
 
-def paciente_menos_dias (pacientes:list[list])->list[list]:
+def paciente_menos_dias (pacientes:list[list]):
     """Busca en la matriz el paciente con menos días de internacion y printea sus datos
 
     Args:
@@ -118,15 +119,15 @@ def paciente_menos_dias (pacientes:list[list])->list[list]:
     cantidad_pacientes = len(pacientes)
     
     for i in range (cantidad_pacientes):
-        if pacientes[i][4]<paciente_menos_dias:
-            paciente_menos_dias=pacientes[i][4]
+        if pacientes[i][4] < paciente_menos_dias:
+            paciente_menos_dias = pacientes[i][4]
             nombre_paciente_menos_dias = pacientes[i][1]
             numero_historial_paciente_menos_dias = pacientes[i][0]
             edad_paciente_menos_dias = pacientes [i][2]
             diagnostico_paciente_menos_dias = pacientes [i][3]
 
     print (f"""El paciente con menos días es {nombre_paciente_menos_dias}, numero clínico {numero_historial_paciente_menos_dias}. Tiene {edad_paciente_menos_dias} años,
-        le diagnosticaron {diagnostico_paciente_menos_dias} y está internado hace {paciente_menos_dias} """)
+    le diagnosticaron {diagnostico_paciente_menos_dias} y está internado hace {paciente_menos_dias} días """)
 
 def cantidad_pacientes_mas_5_dias (pacientes:list[list]):
     """Busca en la matriz a todos los pacientes con más de 5 dias de internacion, printea la cantidad y los nombres de cada uno
@@ -137,19 +138,20 @@ def cantidad_pacientes_mas_5_dias (pacientes:list[list]):
     cantidad_mas_5_dias = 0
     lista_pacientes_mas_de_5_dias = []
     for i in range (len(pacientes)):
-        if pacientes[i][4]>5:
-            cantidad_mas_5_dias+=1
+        if pacientes[i][4] > 5:
+            cantidad_mas_5_dias += 1
             lista_pacientes_mas_de_5_dias.append(pacientes[i][1])
-    print (f"Hay {cantidad_mas_5_dias} pacientes internados hace más de 5 días: ", end= "")
-    contador_escritos=0
+    print (f"Hay {cantidad_mas_5_dias} pacientes internados hace más de 5 días: ", end = "")
+
+    contador_escritos = 0
     for i in range (len(lista_pacientes_mas_de_5_dias)):
-        contador_escritos+=1
-        if contador_escritos==len(lista_pacientes_mas_de_5_dias):
+        contador_escritos += 1
+        if contador_escritos == len(lista_pacientes_mas_de_5_dias):
             print (lista_pacientes_mas_de_5_dias[i])
-        elif contador_escritos==(len(lista_pacientes_mas_de_5_dias)-1):
-            print (lista_pacientes_mas_de_5_dias[i], end= " y ")
+        elif contador_escritos == (len(lista_pacientes_mas_de_5_dias) - 1):
+            print (lista_pacientes_mas_de_5_dias[i], end = " y ")
         else:
-            print (lista_pacientes_mas_de_5_dias[i], end= ", ")  
+            print (lista_pacientes_mas_de_5_dias[i], end = ", ")  
     print (" ")         
 
 def calcular_promedio_internacion(pacientes:list[list]):
@@ -161,6 +163,8 @@ def calcular_promedio_internacion(pacientes:list[list]):
     suma_tiempos_internacion = 0
     for i in range (len(pacientes)):
         suma_tiempos_internacion += pacientes[i][4]
-    promedio_tiempo_internacion= suma_tiempos_internacion/(len(pacientes))
+    promedio_tiempo_internacion = (suma_tiempos_internacion/(len(pacientes)))
+    promedio_tiempo_internacion = round(promedio_tiempo_internacion,2)
 
-    print (f"El tiempo promedio de internación es {promedio_tiempo_internacion}")
+
+    print (f"El tiempo promedio de internación es {promedio_tiempo_internacion} días.")
